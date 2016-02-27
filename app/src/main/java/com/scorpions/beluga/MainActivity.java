ff147906f9d49d7fe5cc477bb8cfc7347c0065be
kinggreenhall <kinggreenhall@gmail.com>
2016-02-27 10:12:34 +0800

    add some icon
    add some icon and debug xjx pull request code.

--- app/src/main/java/com/scorpions/beluga/MainActivity.java
+++ app/src/main/java/com/scorpions/beluga/MainActivity.java
@@ -1,14 +1,10 @@
 package com.scorpions.beluga;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,21 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.io.File;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static int REQ_1 = 1;
-    private static int REQ_2 = 2;
-    private String mFilePath1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mFilePath1 = Environment.getExternalStorageDirectory().getPath();
-        mFilePath1 = mFilePath1 + "/" + "temp1.png";
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -39,7 +29,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "NEw", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Snackbar.make(view, "NEw", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -91,9 +83,11 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        if (id == R.id.nav_home) {
 
-        if (id == R.id.nav_camera) {
-
+        } else if (id == R.id.nav_camera) {
+            Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_video) {
 
         } else if (id == R.id.nav_synthesis) {
@@ -104,8 +98,6 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_setting) {
 
-        }else if (id == R.id.nav_help) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -113,11 +105,6 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public void startCamera(View view){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        Uri photoUri = Uri.fromFile(new File(mFilePath1));
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-        startActivityForResult(intent, REQ_1);
-    }
+
 
 }
